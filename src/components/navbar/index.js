49e6bafabfdec2904f91f0e6feefc247b5c42d0e
rayhan256 <rayhangamawanto@gmail.com>
2021-06-26 15:20:58 +0700

    remove unused link navbar

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Logo from "../../assets/images/logo/logo.svg";
+import { Link } from "react-router-dom";
 import "./index.scss";
 export default function Navbar() {
   return (
@@ -7,14 +8,14 @@ export default function Navbar() {
       <nav className="navbar navbar-light">
         <div className="container-fluid">
           <div className="d-flex flex-row justify-content-around w-100">
-            <a className="navbar-brand" href="#">
+            <Link className="navbar-brand" href="/">
               <img
                 src={Logo}
                 className="d-inline-block align-text-top"
                 alt="Kamarku"
                 width="80"
               />
-            </a>
+            </Link>
             <form>
               <div className="input-group">
                 <i
